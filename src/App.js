ed471177b2a15e38dd8a34736d63ee749450f19f
brantchyoga <brantrporter@gmail.com>
2018-03-06 17:02:28 -0800

    added state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,43 @@ import Authors from './Authors.js';
 
 
 class Post extends Component {
+
+  constructor(props){
+    super()
+    this.state = {
+      body: props.body,
+      currentValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log(this.state)
+    let currentValue = this.state.currentValue
+    this.setState({
+      body: currentValue
+    })
+  }
+
+  handleChange(e){
+    this.setState({
+      currentValue: e.target.value
+    })
+    console.log(e.target.value);
+    console.log(this.state.body);
+  }
+
+  handleClick(e){
+    var updatedBody = prompt('change')
+    this.setState({
+      body: updatedBody
+    })
+  }
   render() {
-    const {title, authors, body, comments} = this.props
+    const {title, authors, comments} = this.props
     let allAuthors = authors.map(function(author, index){
       return <Authors key={index} text={author} id={index+1}/>
     })
@@ -23,7 +58,12 @@ class Post extends Component {
       <div className="text-center">
         <h1 className="title-h1">{title}</h1>
         <h3>Written By: {allAuthors}</h3>
-        <p>{body}</p>
+        <p>{this.state.body}</p>
+
+        <input type="text" onChange={this.handleChange}/>
+        <button onClick={this.handleSubmit}>Edit Post</button>
+        <button onClick={this.handleClick}>Edit By Prompt</button>
+
         <br/>
         <p>Comments:</p>
         <ul>{comments.map(function(comment){
